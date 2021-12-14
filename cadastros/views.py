5bf49b69a16cb9f6a1fcdbe1752d1dcf3fcccb0e
Suelen Almeida <santossuelen492@gmail.com>
2021-12-14 18:56:06 -0300

    adicionado delete e update viwes

--- cadastros/views.py
+++ cadastros/views.py
@@ -42,3 +42,77 @@ class MultaCreate(CreateView):
     fields = ["valor", "data_multa", "pagou"]
     template_name = "cadastros/form.html"
     success_url = reverse_lazy("index")
+
+
+class CategoriaCreate(UpdateView):
+    model = Categoria
+    fields = ["nome"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("index")
+
+
+class ObjetoCreate(UpdateView):
+    model = Objeto
+    fields = ["emprestado", "categoria", "descricao", "informacoes_adcionais"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("index")
+
+
+class EmprestimoCreate(UpdateView):
+    model = Emprestimo
+    fields = ["data_retirada", "data_limite", "data_devolucao", "objeto_emprestado", "usuario"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("index")
+
+
+class PerfilCreate(UpdateView):
+    model = Perfil
+    fields = ["nome", "sobrenome","dt_nascimento", "cpf", "ra", "usuario"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("index")
+
+
+
+
+class MultaCreate(UpdateView):
+    model = Multa
+    fields = ["valor", "data_multa", "pagou"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("index")
+
+
+class CategoriaCreate(DeleteView):
+    model = Categoria
+    fields = ["nome"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("index")
+
+
+class ObjetoCreate(DeleteView):
+    model = Objeto
+    fields = ["emprestado", "categoria", "descricao", "informacoes_adcionais"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("index")
+
+
+class EmprestimoCreate(DeleteView):
+    model = Emprestimo
+    fields = ["data_retirada", "data_limite", "data_devolucao", "objeto_emprestado", "usuario"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("index")
+
+
+class PerfilCreate(DeleteView):
+    model = Perfil
+    fields = ["nome", "sobrenome","dt_nascimento", "cpf", "ra", "usuario"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("index")
+
+
+
+
+class MultaCreate(DeleteView):
+    model = Multa
+    fields = ["valor", "data_multa", "pagou"]
+    template_name = "cadastros/form.html"
+    success_url = reverse_lazy("index")
