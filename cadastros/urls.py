9c806e10da1dcdcce381db71b954e930d086291d
Suelen Almeida <santossuelen492@gmail.com>
2021-12-16 14:49:45 -0300

    atualizado topico 1 e 2 da etapa 3

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,19 +1,38 @@
 from os import name
 from django.urls import path
 from .views import CategoriaCreate, ObjetoCreate, EmprestimoCreate, PerfilCreate, MultaCreate
+from .views import CategoriaUpdate, ObjetoUpdate, EmprestimoUpdate, PerfilUpdate, MultaUpdate
+from .views import CategoriaDelete, ObjetoDelete, EmprestimoDelete, PerfilDelete, MultaDelete
+from .views import CategoriaList, ObjetoList, EmprestimoList, PerfilList, MultaList
+
 
 urlpatterns = [
     # path('endereço', MinhaView.as_view(), name="name-da-url"),
 
     path("cadastrar/categoria/", CategoriaCreate.as_view(), name="cadastrar-categoria"),
-
     path("cadastrar/objeto/", ObjetoCreate.as_view(), name="cadastrar-objeto"),
-
     path("cadastrar/emprestimo/", EmprestimoCreate.as_view(), name="cadastrar-emprestimo"),
-
     path("cadastrar/perfil/", PerfilCreate.as_view(), name="cadastrar-perfil"),
-
-
     path("cadastrar/multa/", MultaCreate.as_view(), name="cadastrar-multa"),
+
+    path("editar/categoria/<int:pk>/", CategoriaUpdate.as_view(), name="editar-categoria"),
+    path("editar/objeto/<int:pk>/", ObjetoUpdate.as_view(), name="editar-objeto"),
+    path("editar/emprestimo/<int:pk>/", EmprestimoUpdate.as_view(), name="editar-emprestimo"),
+    path("editar/perfil/<int:pk>/", PerfilUpdate.as_view(), name="editar-perfil"),
+    path("editar/multa/<int:pk>/", MultaUpdate.as_view(), name="editar-multa"),
+
+    path("excluir/categoria/<int:pk>/", CategoriaDelete.as_view(), name="excluir-categoria"),
+    path("excluir/objeto/<int:pk>/", ObjetoDelete.as_view(), name="excluir-objeto"),
+    path("excluir/emprestimo/<int:pk>/", EmprestimoDelete.as_view(), name="excluir-emprestimo"),
+    path("excluir/perfil/<int:pk>/", PerfilDelete.as_view(), name="excluir-perfil"),
+    path("excluir/multa/<int:pk>/", MultaDelete.as_view(), name="excluir-multa"),
+
+    path("listar/categoria/", CategoriaList.as_view(), name="listar-categoria"),
+    path("listar/objeto/", ObjetoList.as_view(), name="listar-objeto"),
+    path("listar/emprestimo/", EmprestimoList.as_view(), name="listar-emprestimo"),
+    path("listar/perfil/", PerfilList.as_view(), name="listar-perfil"),
+    path("listar/multa/", MultaList.as_view(), name="listar-multa"),
+
+
 ]
 
--- cadastros/views.py
+++ cadastros/views.py
@@ -6,113 +6,96 @@ from django.views.generic.list import ListView
 
 # Create your views here.
 
-
+########CRIAR/INSERIR#######
 class CategoriaCreate(CreateView):
     model = Categoria
     fields = ["nome"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
-
-
+    success_url = reverse_lazy("listar-categoria")
 class ObjetoCreate(CreateView):
     model = Objeto
     fields = ["emprestado", "categoria", "descricao", "informacoes_adcionais"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
-
-
+    success_url = reverse_lazy("listar-objeto")
 class EmprestimoCreate(CreateView):
     model = Emprestimo
     fields = ["data_retirada", "data_limite", "data_devolucao", "objeto_emprestado", "usuario"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
-
-
+    success_url = reverse_lazy("listar-emprestimo")
 class PerfilCreate(CreateView):
     model = Perfil
     fields = ["nome", "sobrenome","dt_nascimento", "cpf", "ra", "usuario"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
-
-
-
-
+    success_url = reverse_lazy("listar-perfil")
 class MultaCreate(CreateView):
     model = Multa
     fields = ["valor", "data_multa", "pagou"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
+    success_url = reverse_lazy("listar-multa")
 
-
-class CategoriaCreate(UpdateView):
+#######ATUALIZAR/ALTERAR#######
+class CategoriaUpdate(UpdateView):
     model = Categoria
     fields = ["nome"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
-
-
-class ObjetoCreate(UpdateView):
+    success_url = reverse_lazy("listar-categoria")
+class ObjetoUpdate(UpdateView):
     model = Objeto
     fields = ["emprestado", "categoria", "descricao", "informacoes_adcionais"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
-
-
-class EmprestimoCreate(UpdateView):
+    success_url = reverse_lazy("listar-objeto")
+class EmprestimoUpdate(UpdateView):
     model = Emprestimo
     fields = ["data_retirada", "data_limite", "data_devolucao", "objeto_emprestado", "usuario"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
-
-
-class PerfilCreate(UpdateView):
+    success_url = reverse_lazy("listar-emprestimo")
+class PerfilUpdate(UpdateView):
     model = Perfil
     fields = ["nome", "sobrenome","dt_nascimento", "cpf", "ra", "usuario"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
-
-
-
-
-class MultaCreate(UpdateView):
+    success_url = reverse_lazy("listar-perfil")
+class MultaUpdate(UpdateView):
     model = Multa
     fields = ["valor", "data_multa", "pagou"]
     template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
+    success_url = reverse_lazy("listar-multa")
 
-
-class CategoriaCreate(DeleteView):
+######DELETAR######
+class CategoriaDelete(DeleteView):
     model = Categoria
-    fields = ["nome"]
-    template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
-
-
-class ObjetoCreate(DeleteView):
+    template_name = "cadastros/form-excluir.html"
+    success_url = reverse_lazy("listar-categoria")
+class ObjetoDelete(DeleteView):
     model = Objeto
-    fields = ["emprestado", "categoria", "descricao", "informacoes_adcionais"]
-    template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
-
-
-class EmprestimoCreate(DeleteView):
+    template_name = "cadastros/form-excluir.html"
+    success_url = reverse_lazy("listar-objeto")
+class EmprestimoDelete(DeleteView):
     model = Emprestimo
-    fields = ["data_retirada", "data_limite", "data_devolucao", "objeto_emprestado", "usuario"]
-    template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
-
-
-class PerfilCreate(DeleteView):
+    template_name = "cadastros/form-excluir.html"
+    success_url = reverse_lazy("listar-emprestimo")
+class PerfilDelete(DeleteView):
     model = Perfil
-    fields = ["nome", "sobrenome","dt_nascimento", "cpf", "ra", "usuario"]
-    template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
-
-
-
-
-class MultaCreate(DeleteView):
+    template_name = "cadastros/form-excluir.html"
+    success_url = reverse_lazy("listar-perfil")
+class MultaDelete(DeleteView):
     model = Multa
-    fields = ["valor", "data_multa", "pagou"]
-    template_name = "cadastros/form.html"
-    success_url = reverse_lazy("index")
+    template_name = "cadastros/form-excluir.html"
+    success_url = reverse_lazy("listar-multa")
+
+########LISTAR#########
+class CategoriaList(ListView):
+    model = Categoria
+    template_name = "cadastros/listas/categoria.html"
+class ObjetoList(ListView):
+    model = Objeto
+    template_name = "cadastros/listas/objeto.html"
+class EmprestimoList(ListView):
+    model = Emprestimo
+    template_name = "cadastros/listas/emprestimo.html"
+class PerfilList(ListView):
+    model = Perfil
+    template_name = "cadastros/listas/perfil.html"
+class MultaList(ListView):
+    model = Multa
+    template_name = "cadastros/listas/multa.html"
+   
